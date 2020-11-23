277d89e0fdc5cc34b313093d1d1f7e9f329a40f8
Deputatov Nikita <deputatovn@yandex.ru>
2020-11-23 17:14:50 +0300

    Fix cb

--- src/info.js
+++ src/info.js
@@ -2,17 +2,17 @@
 import fs from 'fs';
 
 export const compareFileSizes = (filepath1, filepath2, cb) => {
-  fs.stat(filepath1, (err1, { size: filesize1 }) => {
+  fs.stat(filepath1, (err1, stats1) => {
     if (err1) {
       cb(err1);
       return;
     }
-    fs.stat(filepath2, (err2, { size: filesize2 }) => {
+    fs.stat(filepath2, (err2, stats2) => {
       if (err2) {
         cb(err2);
         return;
       }
-      cb(null, Math.sign(filesize1 - filesize2));
+      cb(null, Math.sign(stats1.size - stats2.size));
     });
   });
 };
