69e013fc605deed83a61de4476f171b7929d2ca6
Deputatov Nikita <deputatovn@yandex.ru>
2020-11-23 10:41:32 +0300

    Add tests and solution

---
+++ src/info.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/prefer-default-export */
+import fs from 'fs';
+
+export const compareFileSizes = (filepath1, filepath2, cb) => {
+  fs.stat(filepath1, (err1, { size: filesize1 }) => {
+    if (err1) {
+      cb(err1, null);
+    }
+    fs.stat(filepath2, (err2, { size: filesize2 }) => {
+      if (err2) {
+        cb(err2, null);
+      }
+      cb(null, Math.sign(filesize1 - filesize2));
+    });
+  });
+};
