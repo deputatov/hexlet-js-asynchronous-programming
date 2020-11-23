61b5a3f29cb3bd431a4b75e4a7d8739fc8c2e300
Deputatov Nikita <deputatovn@yandex.ru>
2020-11-23 11:18:36 +0300

    Fix guard expression

--- src/info.js
+++ src/info.js
@@ -4,11 +4,13 @@ import fs from 'fs';
 export const compareFileSizes = (filepath1, filepath2, cb) => {
   fs.stat(filepath1, (err1, { size: filesize1 }) => {
     if (err1) {
-      cb(err1, null);
+      cb(err1);
+      return;
     }
     fs.stat(filepath2, (err2, { size: filesize2 }) => {
       if (err2) {
-        cb(err2, null);
+        cb(err2);
+        return;
       }
       cb(null, Math.sign(filesize1 - filesize2));
     });
